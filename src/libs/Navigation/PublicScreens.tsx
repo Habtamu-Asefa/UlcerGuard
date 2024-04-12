5cc0ca54dac82c42c12db1cf609e11de4374b67c
miedan <miedanbezuayehu@gmail.com>
2024-04-13 01:31:16 +0300

    SMALL FIX

--- src/libs/Navigation/PublicScreens.tsx
+++ src/libs/Navigation/PublicScreens.tsx
@@ -3,14 +3,12 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '../../pages/SignIn';
 import SignUp from '../../pages/SignUp';
 import {CONST} from '../../CONST';
-import Data from '../../pages/Data';
 import OnBoarding from '../../pages/OnBoarding';
 
 const Stack = createNativeStackNavigator();
 
 export default function PublicScreens() {
   return (
-    <Stack.Navigator initialRouteName={CONST.SCREEN.ONBOARDING}>
     <Stack.Navigator initialRouteName={CONST.SCREEN.ONBOARDING}>
       <Stack.Screen
         name={CONST.SCREEN.SIGNIN}
